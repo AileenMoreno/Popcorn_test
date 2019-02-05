6c9c9456d6bdec6c8b0b9fd90e6418285f7bd9ba
AileenMoreno <aileengmp@gmail.com>
2019-02-05 20:15:57 -0300

    Initial Commit

---
+++ Popcorn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Popcorn
+//
+//  Created by Aileen Gabriela Moreno Perez on 2/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
